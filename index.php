4771c2a65f3ee41bd410ae659c0988362ff49584
Ali Mansur <47969541+alimansurali3@users.noreply.github.com>
2019-11-07 09:40:47 +0700

    template

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php include 'part/head.php' ?>
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- Start Header Top Area -->
+    <?php include 'part/header.php' ?>
+    <!-- End Header Top Area -->
+    <!-- Mobile Menu start -->
+    <?php include 'part/navbar_h.php' ?>
+    <!-- Mobile Menu end -->
+    <!-- Main Menu area start-->
+    <?php include 'part/navbar_v.php' ?>
+    <!-- Main Menu area End--> 
+    <!-- Start Footer area-->
+    <?php include 'part/footer.php' ?>
+    <!-- End Footer area-->
+     <?php include 'part/javascript.php' ?>
+</body>
+
+</html>
---
+++ part/navbar_v.php
@@ -0,0 +1,37 @@
+<div class="mobile-menu-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="mobile-menu">
+                        <nav id="dropdown">
+                            <ul class="mobile-menu-nav">
+                                <li><a data-toggle="collapse" data-target="#Charts" href="index.php">Registrasi Pasien</a>
+                                </li>
+                                <li><a data-toggle="collapse" data-target="#demoevent" href="#">Ambulance</a>
+                                    <ul id="demoevent" class="collapse dropdown-header-top">
+                                        <li><a href="index.php">Ambulance Darurat</a></li>
+                                        <li><a href="index.php">Penjemputan Jenazah</a></li>
+                                    </ul>
+                                </li>
+                                <li><a data-toggle="collapse" data-target="#democrou" href="#">Tracking Obat</a>
+                                    <ul id="democrou" class="collapse dropdown-header-top">
+                                        <li><a href="index.php">Input Data Obat</a></li>
+                                        <li><a href="index.php">History Obat</a></li>
+                                    </ul>
+                                </li>
+                                <li><a data-toggle="collapse" data-target="#demolibra" href="index.php">Tambah Dokter</a>
+                                </li>
+                                <li><a data-toggle="collapse" data-target="#demodepart" href="#">Laporan</a>
+                                    <ul id="demodepart" class="collapse dropdown-header-top">
+                                        <li><a href="index.php">Laporan Registrasi Pasien</a></li>
+                                        <li><a href="index.php">Laporan Tracking Obat</a></li>
+                                        <li><a href="index.php">Laporan Ambulance</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
