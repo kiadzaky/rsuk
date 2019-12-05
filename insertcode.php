be6dfbb4056626d9025cbe3d1f8e311e402b7a40
khotimah15 <44369482+khotimah15@users.noreply.github.com>
2019-12-05 07:13:25 +0700

    refi

--- insertcode.php
+++ insertcode.php
@@ -1,8 +1,16 @@
 <?php
+$conn = mysqli_connect("localhost","root","","newrsuk");
+$db = mysqli_select_db($conn,'newrsuk');
 
 if(isset($_POST['insertdata']))
 {
+  $id_dokter = $_POST['id_dokter'];
+  $nama_dokter = $_POST['nama_dokter'];
+  $no_hp = $_POST['no_hp'];
+  $id_poli = $_POST['id_poli'];
+  $id_jadwal = $_POST['id_jadwal'];
 
+  $query = "INSERT INTO dokter (`id_dokter`,`nama_dokter`,`no_hp`,'id_poli','id_jadwal') VALUES ('$id_dokter','$nama_dokter','$no_hp','$id_poli','$id_jadwal')";
   $query_run = mysqli_query($conn, $query);
 
   if($query_run)
