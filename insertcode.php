948f37f86d0f955b26eb1ecdba5b7e602524f0db
khotimah15 <44369482+khotimah15@users.noreply.github.com>
2019-12-05 07:12:59 +0700

    refi

--- insertcode.php
+++ insertcode.php
@@ -1,14 +1,8 @@
 <?php
-$conn = mysqli_connect("localhost","root","","db_rsuk");
-$db = mysqli_select_db($conn,'db_rsuk');
 
 if(isset($_POST['insertdata']))
 {
-  $no_resep = $_POST['no_resep'];
-  $jml_obat = $_POST['jml_obat'];
-  $status = implode(', ', $_POST['status']);
 
-  $query = "INSERT INTO status_obat (`no_resep`,`jml_obat`,`status`) VALUES ('$no_resep','$jml_obat','$status')";
   $query_run = mysqli_query($conn, $query);
 
   if($query_run)
