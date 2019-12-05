afc63d28d7eac4fb5f2b95936ddd8e59b98df729
khotimah15 <44369482+khotimah15@users.noreply.github.com>
2019-12-05 16:30:46 +0700

    khotimah

--- insertcode.php
+++ insertcode.php
@@ -5,7 +5,7 @@ if(isset($_POST['insertdata']))
 {
   $no_resep  = $_POST['no_resep'];
   $jml_obat  = $_POST['jml_obat'];
-  $status    = status_obat(', ', $_POST['status']);
+  $status    = implode(', ', $_POST['status']);  
   $query = "INSERT INTO status_obat ('',`no_resep`,`jml_obat`,`status`) VALUES ('$no_resep','$jml_obat','$status')";
   $query_run = mysqli_query($conn, $query);
   if($query_run)
@@ -17,4 +17,3 @@ if(isset($_POST['insertdata']))
   echo '<script> alert("Data not Saved")</script>';
 }
 }
-?>
