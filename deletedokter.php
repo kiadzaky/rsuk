7ada33fe880a6859bb39ac9d575f2312f74c7548
khotimah15 <44369482+khotimah15@users.noreply.github.com>
2019-12-06 11:56:56 +0700

    khotimah

---
+++ deletedokter.php
@@ -0,0 +1,14 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "db_rsuk");
+$db = mysqli_select_db($conn, 'db_rsuk');
+if (isset($_POST['deletedata'])) {
+  $id = $_POST['delete_id'];
+  $query = "DELETE FROM dokter WHERE id_dokter='$id'";
+  $query_run = mysqli_query($conn, $query);
+  if ($query_run) {
+    echo '<script> alert("Data Delete")</script>';
+    header('Location: index_dokter.php');
+  } else {
+    echo '<script> alert("Data not Delete")</script>';
+  }
+}
