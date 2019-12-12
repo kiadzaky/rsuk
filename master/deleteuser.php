749187cba60d800b1967d3b490c816776b469cb1
khotimah15 <44369482+khotimah15@users.noreply.github.com>
2019-12-12 20:52:33 +0700

    khotimah

--- master/deleteuser.php
+++ master/deleteuser.php
@@ -3,11 +3,11 @@ $conn = mysqli_connect("localhost", "root", "", "db_rsuk");
 $db = mysqli_select_db($conn, 'db_rsuk');
 if (isset($_POST['deletedata'])) {
   $id = $_POST['delete_id'];
-  $query = "DELETE FROM dokter WHERE id_dokter='$id'";
+  $query = "DELETE FROM akun WHERE nik='$id'";
   $query_run = mysqli_query($conn, $query);
   if ($query_run) {
     echo '<script> alert("Data Delete")</script>';
-    header('Location: index_dokter.php');
+    header('Location: index_user.php');
   } else {
     echo '<script> alert("Data not Delete")</script>';
   }
