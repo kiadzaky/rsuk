4996679aa155e31a25aeb566c1a09393fbfc9c25
Kia Dzaky <kkhiahebat@gmail.com>
2019-12-12 15:50:42 +0700

    j

--- antrian/antrian_pasien.php
+++ antrian/antrian_pasien.php
@@ -1,16 +1,16 @@
 <!doctype html>
 <html class="no-js" lang="">
 
-<?php include 'part/head.php' ?>
+<?php include '../part/head.php' ?>
 <body>
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
     <!-- Start Header Top Area -->
-    <?php include 'part/header.php' ?>
+    <?php include '../part/header.php' ?>
     <!-- End Header Top Area -->
     <!-- Mobile Menu start -->
-    <?php include 'part/navbar_h.php' ?>
+    <?php include '../part/navbar_h.php' ?>
     <!-- Mobile Menu end -->
     <!-- Main Menu area start-->
 <?php
@@ -62,12 +62,12 @@
 
 
 
-    <?php include 'part/navbar_v.php' ?>
+    <?php include '../part/navbar_v.php' ?>
     <!-- Main Menu area End--> 
     <!-- Start Footer area-->
-    <?php include 'part/footer.php' ?>
+    <?php include '../part/footer.php' ?>
     <!-- End Footer area-->
-     <?php include 'part/javascript.php' ?>
+     <?php include '../part/javascript.php' ?>
 </body>
 
 </html>
