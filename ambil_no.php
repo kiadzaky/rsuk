4080e9d33e6b96f31f97c8985e218307ee50d344
Kia Dzaky <kkhiahebat@gmail.com>
2019-12-14 10:26:41 +0700

    hapus

--- ambil_no.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include 'config.php';
-$query = $link->query("SELECT *,COUNT(id) as jml FROM `antrian` JOIN pasien on antrian.no_registrasi = pasien.no_registrasi JOIN akun on pasien.nik = akun.nik WHERE status_antrian = '0' ORDER BY no_antrian DESC LIMIT 1");
-$row = mysqli_fetch_assoc($query);
-return $row;
-?>
