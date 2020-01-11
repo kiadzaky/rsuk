b77842d7d6284485441a7eedf67e5a5e45fa5ed5
refiE41171660 <47969645+refiE41171660@refisembilanbelas19@gmail.com.com>
2020-01-11 13:06:52 +0700

    s

--- registrasi_pasien/registrasi_pasien.php
+++ registrasi_pasien/registrasi_pasien.php
@@ -145,11 +145,7 @@ ON dokter.id_poli=poli.id_poli JOIN jadwal ON jadwal.id_jadwal=dokter.id_jadwal"
 									</div>
 								</div>
 							</div>
-							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
-								<div class="breadcomb-report">
-									<button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="notika-icon notika-sent"></i></button>
-								</div>
-							</div>
+							
 						</div>
 					</div>
 				</div>
