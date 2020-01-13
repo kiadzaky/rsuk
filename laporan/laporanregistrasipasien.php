814f7a5fbf481942ad6ce535df1c527113727593
Kia Dzaky <kkhiahebat@gmail.com>
2020-01-14 00:03:35 +0700

    Update laporanregistrasipasien.php

--- laporan/laporanregistrasipasien.php
+++ laporan/laporanregistrasipasien.php
@@ -184,7 +184,7 @@
             <br /><br />
         </div>
         <button type="submit">Tampilkan</button>
-        <a href="laporanregistrasipasien.php">Reset Filter</a>
+        <a href="../index/?laporan=registrasi">Reset Filter</a>
     </form>
     
     </div>
